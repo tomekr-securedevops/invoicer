ac840bb68398dfe09093c2484f66e3419001a719
Tomek Rabczak <tomek.rabczak@gmail.com>
2018-12-24 14:49:07 -0600

    Add X-XSS-Protection

--- main.go
+++ main.go
@@ -220,6 +220,7 @@ func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Security-Policy", "default-src 'self'; child-src 'self;")
 	w.Header().Add("X-Frame-Options", "SAMEORIGIN")
 	w.Header().Add("X-Content-Type-Options", "nosniff")
+	w.Header().Add("X-XSS-Protection", "1")
 	w.Write([]byte(`
 <!DOCTYPE html>
 <html>
